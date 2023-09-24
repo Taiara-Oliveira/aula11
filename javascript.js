653776a24b4fb93059849f5f796f48083daa6b52
Taiara-Oliveira <137340630+Taiara-Oliveira@users.noreply.github.com>
2023-09-24 18:29:05 -0300

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,47 @@
+// alert('Olá Mundo'):
+
+// var nome = prompt('Digite seu nome');
+// alert('Seja Bem-Vinda, '+ nome +' !');
+
+// var n1 = parseInt(prompt('digite o primeiro número:'));
+// var n2 = parseInt(prompt('digite o segundo número:'));
+
+// alert(' A soma é: '+ (n1+n2));
+var n = parseInt(prompt('Digite um número inteiro:'))
+if (n >=0 && n <=101)
+if(n % 2 ==0){
+    alert(' O número está dentro do intervalo de 0 a 100')
+
+        
+}else{
+
+    if(n % 2 ==0)
+    console.log(' O número está fora do intervalo de 0 a 100')
+    alert('O número está fora do intervalo de 0 a 100')
+
+
+}else{
+    if (n <0 && n <100)
+    alert('O número está fora do intervalo de 0 a 100')
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
